cdd9dd3fcabe55444eead679f034f6364e0b628a
leaf4monkey <joc98@163.com>
2017-07-11 16:59:07 +0800

    reformat

--- rules/errors.js
+++ rules/errors.js
@@ -1,11 +1,11 @@
 module.exports = {
     rules: {
         'comma-dangle': ['off', {
-            'arrays': 'always-multiline',
-            'objects': 'always-multiline',
-            'imports': 'always-multiline',
-            'exports': 'always-multiline',
-            'functions': 'always-multiline'
+            arrays: 'always-multiline',
+            objects: 'always-multiline',
+            imports: 'always-multiline',
+            exports: 'always-multiline',
+            functions: 'always-multiline'
         }],
         'no-extra-boolean-cast': 'warn',
     }
